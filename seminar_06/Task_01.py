a9013f009d5c86ae20963aa9f217541463a96419
Mariya <mariya.dunova.95@mail.ru>
2023-02-02 14:55:16 +0700

    Семинар 6, задача 3

--- seminar_06/Task_01.py
+++ seminar_06/Task_01.py
@@ -2,14 +2,6 @@
 # Необходимо вывести элементы исходного списка, значения которых больше предыдущего элемента.
 # Use comprehension.
 
-# from random import sample
-
-# def previous(num):
-#     my_list = sample(range(num * 3), num)
-#     print(my_list)
-#     return [my_list[num] for num in range(1, len(my_list)) if my_list[num] > my_list[num - 1]]
-
-# print(previous(int(input())))
 
 result_list = []
 list = [int(i) for i in input("Введите список чисел: ").split()]
